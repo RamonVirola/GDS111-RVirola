2361d3af770163313e63db61ae106cf6cc13497e
RamonVirola <147515640+RamonVirola@users.noreply.github.com>
2023-10-14 13:48:34 -0400

    update

--- canvas.js
+++ canvas.js
@@ -1,7 +1,6 @@
-console.log('Working?');
-var canvas = document.querySelector('canvas')
+var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
-canvas.innerHeight = window.innerHeight;
+canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 c.fillRect(100, 100, 100, 100);
