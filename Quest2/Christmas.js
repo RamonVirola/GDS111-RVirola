00b532f0301d5161f8ed424c521e3ef16db4c81d
RamonVirola <147515640+RamonVirola@users.noreply.github.com>
2023-10-14 15:20:10 -0400

    Triangles

--- Quest2/Christmas.js
+++ Quest2/Christmas.js
@@ -1,3 +1,13 @@
 var canvas = document.querySelector('canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+c.beginPath();
+c.moveTo(20, 20);
+c.lineTo(20, 100);
+c.lineTo(100, 100);
+c.lineTo(20, 20);
+c.stroke();
+
+
