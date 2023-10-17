e00729b92fcfa3349efaa036897c60fa7be31257
RamonVirola <147515640+RamonVirola@users.noreply.github.com>
2023-10-17 11:17:37 -0400

    week3b.js

---
+++ Week3b.js
@@ -0,0 +1,20 @@
+console.log("I'm inside Week3 B JavaScript file.");
+
+var health = 100;
+console.log(health);
+console.log("health = " + health);
+
+health = prompt("Type in the number of health");
+console.log("The new entered health = " + health);
+
+var a = 0;
+var b = 0;
+var sum = 0;
+
+a = prompt("Eneter a value: ");
+a = Number(a);
+b = prompt("Type a second value:");
+b = Number(b);
+sum = a + b;
+console.log("The sum of a & b is = " + sum);
+
