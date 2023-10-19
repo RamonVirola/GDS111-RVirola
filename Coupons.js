2287ee8659eb267a1cf2771e5ba6c039c5a09a7c
RamonVirola <147515640+RamonVirola@users.noreply.github.com>
2023-10-19 11:48:05 -0400

    coupon

---
+++ Coupons.js
@@ -0,0 +1,18 @@
+
+var c = Number(prompt("how many coupons? "));
+var b = 10;
+var g = 3;
+
+var difference = 0;
+
+c % w
+
+
+
+if (w>c) {
+	difference = w - c;
+} else {
+	difference = w - g;
+}
+
+console.log("The diiference = " = difference );
