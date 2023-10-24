6ca656dce46bcc88b270b7b865cd3ccb51166800
RamonVirola <147515640+RamonVirola@users.noreply.github.com>
2023-10-24 09:07:56 -0400

    ?

---
+++ week4/Week4a.js
@@ -0,0 +1,8 @@
+var wideSCOPE = 7;
+if (wideSCOPE == 7) {
+    let localSCOPE = 10
+    console.log("localSCOPE inside if-block = " + localSCOPE);
+    comsole.log("wideSCOPE inside if-block = " + wideSCOPE);
+}
+console.log("localSCOPE outside if-block = " + localSCOPE);
+console.log("localSCOPE outside if-block = " + wideSCOPE);
