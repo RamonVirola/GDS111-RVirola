c229917baa30cb942d46002e13ca623f894d98a8
RamonVirola <147515640+RamonVirola@users.noreply.github.com>
2023-11-05 14:43:22 -0500

    Russ, Roul

---
+++ Week5a.2.js
@@ -0,0 +1,11 @@
+var b = Math.floor(Math.random() * 6) + 1;
+var p = Math.floor(Math.random() * 6) + 1;
+console.log(b);
+console.log(p);
+
+    if (p == b){
+        console.log("Game over");     
+    }else{
+        console.log("your safe")
+    }
+
