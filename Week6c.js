72235edc6e8b57172238999bda7dea9ca3189cb3
RamonVirola <147515640+RamonVirola@users.noreply.github.com>
2023-11-09 11:16:16 -0500

    Week6

---
+++ Week6c.js
@@ -0,0 +1,31 @@
+myFunction(); // callers
+func2(7);
+
+let twice = doubleIt(5);
+
+console.log("Functions doubleIt() returns = " + twice);
+
+let msg = message();
+console.log(msg);
+
+// callee
+function myFunction() {
+    console.log("I'm inside myFunction()");
+}
+
+function func2(param1) {
+    console.log(param1);
+    console.log("Inside func2");
+}
+
+function doubleIt(aNumber) {
+    let d = aNumber * 2;
+    return d; 
+}
+
+function message() {
+    return "This is a message";
+}
+
+
+
