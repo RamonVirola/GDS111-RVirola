8b3671345daadcb1628efe9e91319bbf986909b6
ramon Virola <rvirola@email.neit.edu.com>
2023-12-13 19:11:18 -0500

    FizzBuzz

---
+++ fizbuzz.js
@@ -0,0 +1,14 @@
+let start = Number(prompt("Type start number: "));
+let end = Number(prompt("Type end number: "));
+
+for (let i = start; i <= end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
